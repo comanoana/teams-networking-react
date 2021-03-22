365ec828b1826d3ce3930a0955c0cbfa45fc544f
comanoana <oanamariarodila95@yahoo.es>
2021-03-22 20:04:20 +0200

    add Component as class + internal state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
+import { Component } from 'react';
 import './App.css';
 import {PersonsTable} from "./PersonsTable";
 
 
-let persons = [
- { 
-    "id": "a123",
-    "firstName": "Oana",
-    "lastName":"Coman",
-    "url": "https://github.com/"
-},
-{  
-   "id": "b654",
-    "firstName": "Paul",
-    "lastName":"Coman",
-    "url": "https://github.com/"
 
-}
-];
 
-function App() {
-  return (
-   <div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       persons : []
+    }
+  }
+     componentDidMount(){
+      console.warn('mount');
+      setTimeout(() => {
+        console.warn("loaded");
+        this.setState({
+          persons:[
+            { 
+              "id": "a123",
+              "firstName": "Oana",
+              "lastName":"Coman",
+              "url": "https://github.com/"
+          },
+          {  
+             "id": "b654",
+              "firstName": "Paul",
+              "lastName":"Coman",
+              "url": "https://github.com/"
+          
+          }
+          ]
+        })
+      }, 2000);
+      
+    }   
+
+   render() {
+     console.warn(this.state.persons);
+     return(
+    <div>
     <h1>Teams Networking</h1>
     <div>Search</div>
-    <PersonsTable persons={persons}  border={1}/>
+    <PersonsTable persons={this.state.persons}  border={1} />
     </div>
-  )}
+   )}
+  }
+  
 
 export default App;
  
