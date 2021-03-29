d4891be7ad4761bc8963eb513c47384603ca2f9b
comanoana <oanamariarodila95@yahoo.es>
2021-03-29 17:05:50 +0300

    add to API

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import {PersonsTable} from "./PersonsTable";
 
 
 class App extends Component {
-  constructor(persons) {
-    super(persons);
+  constructor(props) {
+    super(props);
     this.state = {
        persons : [],
        date: new Date().toString()
@@ -35,19 +35,65 @@ class App extends Component {
         });
       }
        
+      add(person) {
+        console.warn('person', person);
+        document.getElementById('main-form').reset();
+    
+        fetch("http://localhost:3000/teams-json/create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(person)
+        })
+          .then(res => res.json())
+          .then(r => {
+            console.warn(r);
+            if (r.success) {
+              person.id = r.id;
+              const persons= this.state.persons.concat(person);
+              this.setState({
+               persons
+              });
+              //this.load();
+            }
+          });
+      }
+    
+      remove(id) {
+        fetch("http://localhost:3000/teams-json/delete", {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ id })
+        }).then(r => r.json()).then(status => {
+          this.load();
+        });
+      }
+      
 
-   render() {
-     console.debug(this.state.persons);
-     return(
-    <div>
-    <h1>Teams Networking</h1>
-    <div>Search</div>
-    <PersonsTable persons={this.state.persons}  border={1} />
-    <div>{this.state.date}</div>
-    </div>
-   );
-  }
-  }
+      render() {
+        console.debug(this.state.persons);
+        return (
+          <div>
+            <h1>Teams Networking</h1>
+            <div>Search</div>
+            <PersonsTable 
+              persons={this.state.persons}
+              border={1}
+              onSubmit={person => {
+                this.add(person);
+              }}
+              onDelete={id => {
+                this.remove(id);
+              }}
+            />
+            <div>{this.state.date}</div>
+          </div>
+        );
+      }
+    }
   
 
 export default App;
--- src/PersonsTable.js
+++ src/PersonsTable.js
@@ -1,33 +1,54 @@
-export const PersonsTable = ({border, persons}) => (
-<table border={border}>
-    <thead>
-        <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Link</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody>
-        {persons.map((person, index) => (
-            <tr key={index}>
-                <td>{person.firstName.split(/\s*,\s*/).join("<br>")}</td>
-                <td>{person.lastName}</td>
-                <td><a target="_blank" href={person.url}>GitHub</a></td>
-                <td>
-                    <a href="#" className="delete-row" data-id="{person.id}">&#10006;</a>
-                    <a href="#" className="edit-row" data-id="{person.id}">&#9998;</a>
-                </td>
-            </tr>
-    ))}
-    </tbody>
-    <tfoot>
-        <tr>
-            <td><input type="text" placeholder="Enter First Name" name="firstName" /></td>
-            <td><input type="text" placeholder="Enter Last Name" name="lastName" /></td>
-            <td><input type="text" placeholder="GitHub account" name="gitHub" /></td>
-            <td><button>Save</button></td>
-        </tr>
-    </tfoot>
+function getValues(){
+    const firstName = document.querySelector("input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+    const url= document.querySelector("input[name=url]").value;
+
+    const person = {
+       firstName,
+       lastName,
+      url
+};
+return person;
+}
+
+export const PersonsTable = ({border, persons, onSubmit, onDelete  }) => (
+  <form id="main-form" onSubmit={ e =>  {
+      e.preventDefault();
+     const values= getValues();
+     onSubmit(values)
+  }}>
+      <table border={border}>
+  <thead>
+      <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Link</th>
+          <th></th>
+      </tr>
+  </thead>
+  <tbody>
+      {persons.map((person, index) => (
+          <tr key={index}>
+              <td>{person.firstName.split(/\s*,\s*/).join("<br>")}</td>
+              <td>{person.lastName}</td>
+              <td><a target="_blank" href={person.url}>GitHub</a></td>
+              <td>
+                  <a href="#" className="delete-row" onClick={e => {
+                      onDelete(person.id);
+                  }}>&#10006;</a>
+                  <a href="#" className="edit-row" data-id="{person.id}">&#9998;</a>
+              </td>
+          </tr>
+  ))}
+  </tbody>
+  <tfoot>
+      <tr>
+          <td><input type="text" requiered name="firstName" placeholder="Enter First Name"  /></td>
+          <td><input type="text" requiered name="lastName" placeholder="Enter Last Name" /></td>
+          <td><input type="text" requiered name="url" placeholder="GitHub account"  /></td>
+          <td><button type="submit">Save</button></td>
+      </tr>
+  </tfoot>
 </table>
+</form>
 );
